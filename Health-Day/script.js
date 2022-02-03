dc44be7d1c2ffdebefe5714fb5893b58fa781e9a
MATTHEW MASETOR <matvei.matyuk@gmail.com>
2022-02-04 00:31:47 +0300

    feax: update files

---
+++ Health-Day/script.js
@@ -0,0 +1,21 @@
+const play_video = document.querySelector(".play-video-ferst");
+const pause_video = document.querySelector(".pause-video-ferst");
+
+let video;
+
+video = document.querySelector(".video");
+
+play_video.addEventListener('click', function (){
+    video.play()
+    play_video.style.display = 'none';
+    pause_video.style.display = 'block';
+    console.log('2')
+});
+
+
+pause_video.addEventListener('click', function (){
+    video.pause()
+    play_video.style.display = 'block';
+    pause_video.style.display = 'none';
+    console.log('1')
+});
