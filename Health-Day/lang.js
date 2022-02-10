872d7899194b5fc8aa9b85bdfcdbea8da9283e4b
MATTHEW MASETOR <matvei.matyuk@gmail.com>
2022-02-11 01:14:37 +0300

    feax: update files

--- Health-Day/lang.js
+++ Health-Day/lang.js
@@ -1,45 +1,7 @@
-const play_video = document.querySelector(".play-video-ferst");
-const pause_video = document.querySelector(".pause-video-ferst");
-
-let video;
-
-video = document.querySelector(".video");
-
-play_video.addEventListener('click', function (){
-    video.play()
-    play_video.style.display = 'none';
-    pause_video.style.display = 'block';
-    console.log('2')
-});
-
-
-pause_video.addEventListener('click', function (){
-    video.pause()
-    play_video.style.display = 'block';
-    pause_video.style.display = 'none';
-    console.log('1')
-});
-
-
-
-// Jquery
-
-$(document).ready(function(){
-    $('.slider-box').slick({
-        arrows:true,
-        slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    });
-});
-
-// change language
-
 const langArr = {
-    "day-this" : {
-        "ru": "День здоровья —<br>Это прекрасно",
-        "en": "Health Day —<br>This pearfect",
+    "day" : {
+        "ru": "День здоровья — Это прекрасно",
+        "en": "Health Day — This pearfect",
     },
     "title-box-what" : {
         "ru": "Что такое день здоровья?",
@@ -58,12 +20,12 @@ const langArr = {
         "en": "China is considered the birthplace of reflexology. It is based on the idea that the body is a single, interconnected system, and a malfunction of one part immediately responds to another. The channels located on the arms are connected to the internal organs of your body. Changes in internal organs are reflected in the hands. By stimulating the acupuncture points on the palm of your hand, you can understand about the presence or absence of a disease or any changes in the organs. The Tiens-life acupuncture device helps and facilitates our task of determining the state of organs with great accuracy.",
     },
     "second-text-tiens" : {
-        "ru": "<b>Принцип действия Тиенс-лайф:</b> Благодаря воздействию электромагнитной терапии на акупунктурные точки на ладони, прибор выявляет состояние здоровья органов в теле человека. В зависимости от уровня болевых ощущений можно определить есть ли физиологические отклонения в органах.",
-        "en": "<b>The principle of operation of Tiens-life:</b> Due to the effect of electromagnetic therapy on acupuncture points on the palm of the hand, the device detects the state of health of organs in the human body. Depending on the level of pain, it is possible to determine whether there are physiological abnormalities in the organs.",
+        "ru": "Принцип действия Тиенс-лайф: Благодаря воздействию электромагнитной терапии на акупунктурные точки на ладони, прибор выявляет состояние здоровья органов в теле человека. В зависимости от уровня болевых ощущений можно определить есть ли физиологические отклонения в органах.",
+        "en": "The principle of operation of Tiens-life: Due to the effect of electromagnetic therapy on acupuncture points on the palm of the hand, the device detects the state of health of organs in the human body. Depending on the level of pain, it is possible to determine whether there are physiological abnormalities in the organs.",
     },
     "threeth-text-tiens" : {
-        "ru": "<b>Преимущества:</b>",
-        "en": "<b>Advantages:</b>",
+        "ru": "Преимущества:",
+        "en": "Advantages:",
     },
     "lang01" : {
         "ru": "Отличается высоким качеством и простотой использования.",
@@ -82,8 +44,8 @@ const langArr = {
         "en": "Safe and reliable! Testing and recovery takes place without disturbing the skin.",
     },
     "fourth-text-tiens" : {
-        "ru": "<b>Выгоды:</b>",
-        "en": "<b>Benefits:</b>",
+        "ru": "Выгоды:",
+        "en": "Benefits:",
     },
     "lang05" : {
         "ru": "Вы экономите свое время посетив всего лишь одного специалиста!",
@@ -106,54 +68,3 @@ const langArr = {
         "en": "And most importantly, get recommendations for improving the state of the body and well-being!",
     },
 }
-
-const language = document.querySelector(".en");
-const language01 = document.querySelector(".ru");
-const allLang = ['en', 'ru'];
-
-
-language.addEventListener('click', changeURLLanguage);
-language01.addEventListener('click', changeURLLanguage01);
-
-function changeURLLanguage(){
-    location.href = window.location.pathname + '#' + 'en';
-    location.reload();
-    language.style.color = gray;
-}
-
-function changeURLLanguage01(){
-    location.href = window.location.pathname + '#'+ 'ru';
-    location.reload();
-
-}
-
-function changeLanguage(){
-    let hash = window.location.hash
-    hash = hash.substr(1);
-    console.log(hash)
-    if (!allLang.includes(hash)){
-        location.href = window.location.pathname + '#en';
-        location.reload();
-    }
-    language.value = hash;
-    language01.value = hash;
-    document.querySelector('.day-this').innerHTML = langArr['day-this'][hash];
-    document.querySelector('.title-box-what').innerHTML = langArr['title-box-what'][hash];
-    document.querySelector('.text-day').innerHTML = langArr['text-day'][hash];
-    document.querySelector('.title-box-video').innerHTML = langArr['title-box-video'][hash];
-    document.querySelector('.ferst-text-tiens').innerHTML = langArr['ferst-text-tiens'][hash];
-    document.querySelector('.second-text-tiens').innerHTML = langArr['second-text-tiens'][hash];
-    document.querySelector('.threeth-text-tiens').innerHTML = langArr['threeth-text-tiens'][hash];
-    document.querySelector('.lang01').innerHTML = langArr['lang01'][hash];
-    document.querySelector('.lang02').innerHTML = langArr['lang02'][hash];
-    document.querySelector('.lang03').innerHTML = langArr['lang03'][hash];
-    document.querySelector('.lang04').innerHTML = langArr['lang04'][hash];
-    document.querySelector('.fourth-text-tiens').innerHTML = langArr['fourth-text-tiens'][hash];
-    document.querySelector('.lang05').innerHTML = langArr['lang05'][hash];
-    document.querySelector('.lang06').innerHTML = langArr['lang06'][hash];
-    document.querySelector('.lang07').innerHTML = langArr['lang07'][hash];
-    document.querySelector('.lang08').innerHTML = langArr['lang08'][hash];
-    document.querySelector('.lang09').innerHTML = langArr['lang09'][hash];
-}
-
-changeLanguage();
